4d68e9968b69276ae7e5e6d6fb35e96e71c82e2f
Valtteri Ranta <waltherbeach@gmail.com>
2018-10-06 11:44:54 +0300

    merge sort with python

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,5 +1,41 @@
 import random
 
-unsorted = random.sample(range(100), 10)
 
+def merge_sort(sort_list):
+	# Base case, a list of one or no elements:
+	if (len(sort_list) <= 1):
+		return sort_list
+
+	# Recursive case, dividing list into two halves:
+	left_list = sort_list[:len(sort_list)//2]
+	right_list = sort_list[len(sort_list)//2:]
+
+	# Recursively sort both sublists.
+
+	left_list = merge_sort(left_list)
+	right_list = merge_sort(right_list)
+
+	return merge(left_list, right_list)
+
+
+def merge(left, right):
+	result = []
+
+	while (len(left) > 0 and len(right) > 0):
+		if (left[0] <= right[0]):
+			result.append(left.pop(0))
+		else:
+			result.append(right.pop(0))
+
+	while (len(left) > 0):
+		result.append(left.pop(0))
+	while (len(right) > 0):
+		result.append(right.pop(0))
+
+	return result
+
+
+unsorted = random.sample(range(100), 10)
 print(unsorted)
+sorted_list = merge_sort(unsorted)
+print(sorted_list)
